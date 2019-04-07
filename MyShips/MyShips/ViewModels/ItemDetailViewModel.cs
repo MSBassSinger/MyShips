575631ccd70510b8d35aaa8db1d93212e574394f
Jeff Jones <msbasssinger@comcast.net>
2019-04-06 21:25:58 -0400

    Initial code

---
+++ MyShips/MyShips/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyShips.Models;
+
+namespace MyShips.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
